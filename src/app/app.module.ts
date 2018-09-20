9fd04660d6d1acb6466e4bb48611f3a8b43677bc
Diego Moreno <diegomorenoa@unisangil.edu.co>
2018-09-20 18:12:04 -0500

    AngularFire2 Added and Login & Register Page Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+import { FIREBASE_CONFIG } from '../config/firebase.config';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +21,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
